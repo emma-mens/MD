d9f28b66052c62fee1c487c7eea4e1e95d30cb07
Emmanuel M Azuh <emazuh@mac-56-129-12.mit.edu>
2018-05-03 16:52:10 -0400

    wip

--- P_and_U.m
+++ P_and_U.m
@@ -1,4 +1,15 @@
 function [P,U]=P_and_U(N,Vs,rij,Fij,Uij,T_inst,P_LRC,E_LRC)    
 
-P = N*kB*T/Vs + .. + P_LRC
-U = E_LRC
+% TODO (emazuh): Optimize this code
+P_tensor = zeros(3,3);
+
+for i=1:N
+    for j=1:N
+        if j>1
+            P_tensor = P_tensor + rij(i,j)'*Fij(i,j);
+        end
+    end
+end
+
+P = (trace(P_tensor)/3 + P_LRC + N*kB*T_inst)/Vs;
+U = E_LRC + mean2(Uij); %TODO: emazuh 
--- force_calculation.m
+++ force_calculation.m
@@ -3,12 +3,10 @@ function [Fij,Uij,rij]=force_calculation(N,r,Ls,rc2)
 % Derivative of potential
    size(r)
    Fij = zeros(N,N,3);
-   
-   %% 
+ 
    % Force b/n particle i and j
    fij = 48*(rij^(-14) - 0.5*);
    
-   %%
    Uij = zeros(N,N);
    rij = zeros(N,N,3);
    
