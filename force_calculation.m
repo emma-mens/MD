e4b6e214fd6c76e40a34f7a1d28a6f0a40fe6441
Emmanuel M Azuh <emazuh@mit.edu>
2018-05-12 15:57:28 -0400

    vectorized F and U

--- force_calculation.m
+++ force_calculation.m
@@ -1,45 +1,30 @@
 function [Fij,Uij,rij]=force_calculation(N,r,Ls,rc2)
-
-   Uij = zeros(N,N);
-
-   % Derivative of potential
-   Fij = zeros(N,N,3);
  
    % Force b/n particle i and j
    epsilon = 1.65*10^(-21); % LJ energy [J]
    sigma = 3.4*10^(-10); % LJ diameter [m]
-   rc = rc2^0.5;
    
-   % calculate distance b/n atom pairs
+   % calculate distance vectors and magnitudes b/n atom pairs
    rij  = permute(r - reshape(r', [1,3,N]), [1,3,2]);
-   
-   % TODO (emazuh): optimize force calculation, account for r cutoff
-   for i=1:N
-       for j=1:N
-           if i < j
-               
-               % set distance
-               rij(i,j,:) = r(i,:)-r(j,:);
-               rij(j,i,:) = - rij(i,j,:);
+   rij_mag = vecnorm(rij, 2, 3);
 
-               % find magnitude
-               r_ij = norm(r(i,:)-r(j,:));
-               
-               % cut off radius
-               if r_ij < rc
-                   s_over_r = sigma/r_ij;
-                   
-                   % calculate force (equal and opposite)
-                   Fij(i,j,:) = rij(i,j,:)*48*epsilon*((s_over_r)^12 - 0.5*(s_over_r)^6)/(r_ij^2);
-                   Fij(j,i,:) = - Fij(i,j,:);
-                   
-                   % LJ potential
-                   Uij(i,j) = 4*epsilon*(s_over_r^12 - s_over_r^6);
-                   Uij(j, i) = Uij(i, j);
-               end
-           end
-       end
-   end
-  
+
+    % since I divide by the magnitude, I'll replace 0 magnitudes with 1
+    % this is fine since the distance vector will still be zero for same atoms
+    rij_mag(eye(N) == 1) = 1;
+
+    s_r6 = (sigma./rij_mag).^6;
+    s_r12 = s_r6.^2;
+
+    Fij = (48 * epsilon * (s_r12 - 0.5*s_r6)./(rij_mag.^2)).*rij;
+
+
+    Uij = 4*epsilon*(s_r12-s_r6);
+
+    % because i hacked the distance matrix (0-> 1), i'll set self potentials to zero
+    Uij(eye(N) == 1) = 0;
+
+
+z
 
 
