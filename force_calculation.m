e326d39b97eb6f2a293789528cde2bba622752bc
Emmanuel M Azuh <emazuh@mit.edu>
2018-05-16 03:13:23 -0400

    probably fixed boundary conditions; velocity initialization might be wrong

--- force_calculation.m
+++ force_calculation.m
@@ -2,16 +2,27 @@ function [Fij,Uij,rij]=force_calculation(N,r,Ls,rc2)
  
    % epsilon = 1.65*10^(-21); % LJ energy [J]
    %sigma = 3.4*10^(-10); % LJ diameter [m]
-
+   
    % calculate distance vectors and magnitudes b/n atom pairs
-   rij  = permute(r - reshape(r', [1,3,N]), [1,3,2]);
-   rij_mag = vecnorm(rij, 2, 3);
+   rij_init  = permute(r - reshape(r', [1,3,N]), [1,3,2]);
+   rp = permute(r - reshape((r+Ls)', [1,3,N]), [1,3,2]);
+   rm = permute(r - reshape((r-Ls)', [1,3,N]), [1,3,2]);
+   rfinal = rij_init.*(abs(rij_init) < abs(rp)) + rp.*(abs(rp) < abs(rij_init));
+   rij = rfinal.*(abs(rfinal) < abs(rm)) + rm.*(abs(rm) < abs(rfinal));
+   
+   % set self distances to zero and distances between corner atoms to
+   % rij (without any offset since an offset will cause overlapping)
+   rij(rij == 0) = rij_init(rij == 0);
+   rij(eye(N) == 1) = 0;
+   
+   rij_mag = vecnorm(rij, 2, 3)
+
+   % rx_mag = 
 
    % since I divide by the magnitude, I'll replace 0 magnitudes with 1
-   % this is fine since the distance vector will still be zero for same atoms
+   % this is fine since s_r12 - s_r6 will still be zero for same atoms
    rij_mag(eye(N) == 1) = 1;
 
-%    s_r6 = (sigma./rij_mag).^6;
    s_r6 = (1./rij_mag).^6;
    s_r12 = s_r6.^2;
 
@@ -19,8 +30,27 @@ function [Fij,Uij,rij]=force_calculation(N,r,Ls,rc2)
 
    Uij = 4*(s_r12-s_r6);
 
-   % because i hacked the distance matrix (0-> 1), i'll set self potentials to zero
-   % Uij(eye(N) == 1) = 0;
 
 
-
+%    rx = r(:,1); ry = r(:,2); rz = r(:,3);
+%    rij = zeros(N,N,3);
+%    
+%    % rij between mirros
+%    % For x: do x + Ls, x - Ls,
+%    rxplus = rx + Ls; rxminus = rx - Ls; ryplus = ry + Ls; ryminus = ry - Ls;
+%    rzplus = rz + Ls; rzminus = rz - Ls;
+% 
+%    rijx = permute(rx - reshape(rx', [1,1,N]), [1,3,2]);
+%    rijxp = permute(rx - reshape(rxplus', [1,1,N]), [1,3,2]);
+%    rijxm = permute(rx - reshape(rxminus', [1,1,N]), [1,3,2]);
+%    
+%    rijy = permute(ry - reshape(ry', [1,1,N]), [1,3,2]);
+%    rijyp = permute(ry - reshape(ryplus', [1,1,N]), [1,3,2]);
+%    rijym = permute(ry - reshape(ryminus', [1,1,N]), [1,3,2]);
+%    
+%    % select images closest to any given atom
+%    tmpx = rijx.*(abs(rijx) < abs(rijxp)) + rijxp.*(abs(rijxp) < abs(rijx));
+%    tmpx = tmpx.*(abs(tmpx) < abs(rijxm)) + rijxm.*(abs(rijxm) < abs(tmpx))
+%    
+%    tmpy = rijy.*(abs(rijy) < abs(rijyp)) + rijyp.*(abs(rijyp) < abs(rijy));
+%    tmpy = tmpy.*(abs(tmpy) < abs(rijym)) + rijym.*(abs(rijym) < abs(tmpy))
--- take_one_step.m
+++ take_one_step.m
@@ -1,50 +1,33 @@
 function [rnew,vnew,T_inst]=take_one_step(N,r,v,F,t,dt,Ls,STEPS_thermostat,alpha,Ts)
 
-m = 1; %39.948*1.660538921*10^(-27); % molecule mass [kg]
-% kB = 1.38065*10^(-23); % Boltzmann constant [J/K]
-% sigma = 3.4*10^(-10); % LJ diameter [m]
-% epsilon = 1.65*10^(-21); % LJ energy [J]
-rc = 3;
-rc2 = rc^2;
-% Ls = Ls*sigma; % TODO (emazuh): change this if you use LJ Ls in force_calculation
+    m = 1; 
 
-% calculate acceleration
-a = F/m;
+    rc = 3;
+    rc2 = rc^2;
 
-% TODO (emazuh): velocity verley (maybe switch with leapfrog later?)
+    % calculate acceleration
+    a = F/m;
 
-% update positions
-rnew = r + v*dt + 0.5*a*dt^2;
+    % update positions
+    vhalf = v + 0.5*a*dt;
+    rnew = r + vhalf*dt;
 
-% apply boundary condition on rnew
-rnew(rnew < 0 | rnew > Ls) = mod(rnew(rnew < 0 | rnew > Ls), Ls);
-% assert(rnew(rnew >= 0) & rnew(rnew <= Ls), 'atoms are outside the box');
+    % apply boundary condition on rnew
+    rnew(rnew < 0 | rnew > Ls) = mod(rnew(rnew < 0 | rnew > Ls), Ls);
 
-[Fij, ~, ~] = force_calculation(N,rnew,Ls, rc2);
-F_next = squeeze(sum(Fij, 2));
+    [Fij, ~, ~] = force_calculation(N,rnew,Ls, rc2);
+    F_next = squeeze(sum(Fij, 2));
+    z
+    % update velocities
+    anew = F_next/m;
+    vnew = v + 0.5*(a + anew)*dt;
 
-% update velocities
-anew = F_next/m;
-vnew = v + 0.5*(a + anew)*dt;
-z
-%V = (Ls*sigma)^3;
-%rho = m*N/V;
-% u = m*sum(vnew, 1)/rho/V; % flow velocity
-u = mean(vnew, 1);
+    % assert(rnew(rnew >= 0) & rnew(rnew <= Ls), 'atoms are outside the box');
 
-% Ts = 1.171461; % desired (and initial) temperature in LJ units (Ts = kB*T/epsilon)
-% T_set = Ts*epsilon/kB; % set temperature, in Kelvin
-% TODO (emazuh): apply thermostat (also check if t < STEPS_thermostat
-% d(zeta)/dt = 6NkBT/Q *(T_set - T_inst)
-% ma = f - zeta*p
+    u = mean(vnew, 1);
 
-% p = m*norm(vnew).^2-norm(u)^2;
+    T_inst = m*sum(vecnorm(vnew, 2, 2).^2-norm(u)^2, 1)/(3*N);
 
-
-T_inst = m*sum(vecnorm(vnew, 2, 2).^2-norm(u)^2, 1)/(3*N);
-
-if t < STEPS_thermostat
-   vnew = vnew*sqrt(Ts/T_inst); 
-end
-
-% T_inst = T_inst*kB/epsilon; % LJ    
+    if t < STEPS_thermostat
+       vnew = vnew*sqrt(Ts/T_inst); 
+    end
